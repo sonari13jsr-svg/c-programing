b1fb372710d5f2e316fc145e937968892be08ea0
sonari13jsr-svg <sonari13jsr@gmail.com>
2025-10-07 15:28:01 +0530

    Create first.c

---
+++ first.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int x=1;
+    int exp= 2 < !0 +7 /10 *3;
+    if(exp) {
+        printf("yes");
+    }
+    else {
+        printf("no");
+    }
+    return 0;
+}
+    
